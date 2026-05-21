62c29c0feab60d01174407ef84bf0cb0a87bf644
elc <elcmardeguzman10@gmail.com>
2026-05-22 03:33:12 +0800

    Update PlanVisitModal.tsx

--- src/components/PlanVisitModal.tsx
+++ src/components/PlanVisitModal.tsx
@@ -10,16 +10,27 @@ interface PlanVisitModalProps {
 
 const expectations = [
   {
-    title: 'Kids',
-    desc: "Check-in is fast and secure. We'll have a host waiting to show you the way.",
-    icon: 'M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
+    title: 'Know God',
+    desc: "Every Sunday is an invitation.",
+    icon: 'M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253',
   },
   {
-    title: 'The Vibe',
-    desc: "Coffee's on us. Dress is 'come as you are.' Service lasts about 75 minutes.",
-    icon: 'M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z',
+    title: 'Find Freedom',
+    desc: "You were never meant to do life alone.",
+    icon: 'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z',
   },
+  {
+    title: 'Discover Purpose',
+    desc: "You were made for more.",
+    icon: 'M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7',
+  },
+  {
+    title: 'Make a Difference',
+    desc: "Your life can change others.",
+    icon: 'M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z',
+  }
 ];
+
 
 const PlanVisitModal = ({ isOpen, onClose }: PlanVisitModalProps) => {
   const vm = usePlanVisitViewModel();
